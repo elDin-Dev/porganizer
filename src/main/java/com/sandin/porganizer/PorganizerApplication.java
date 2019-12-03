76cec03b05eb1d354b26a58e956ab3ee632d1f9a
elDin-Dev <alexmaildese-dev@yahoo.com>
2019-12-04 00:06:26 +0100

    add README.md

---
+++ src/main/java/com/sandin/porganizer/PorganizerApplication.java
@@ -0,0 +1,15 @@
+package com.sandin.porganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorganizerApplication.class, args);
+	}
+
+	
+	
+}
---
+++ src/test/java/com/sandin/porganizer/PorganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandin.porganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
